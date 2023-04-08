69d2365e6dab63a2243fb95c9a37dd68e3ad783d
zaicampo <zaira.campo@yahoo.com>
2023-04-08 12:37:39 -0500

    Default city defined

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [ready, setReady] = useState(false);
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
@@ -82,8 +82,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "df0a2843c2fa36a73178f1e35e3378f6";
-    let city = "Bogota";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultcity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
